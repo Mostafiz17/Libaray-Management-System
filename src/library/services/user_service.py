9e92b5327aae6c9ce0b9bd3bacde7329990941fd
Mostafiz <rbmostafiz211@gmail.com>
2026-09-06 12:42:35 +0600

    json done

--- src/library/services/user_service.py
+++ src/library/services/user_service.py
@@ -1,9 +1,12 @@
 from ..models.user import User
+from ..storage.json_storage import JsonStorage
+
 
 class UserService:
 
     def __init__(self):
         self.users = []
+        self.storage = JsonStorage("data/users.json")
 
     def add_user(self, user):
         self.users.append(user)
@@ -12,5 +15,37 @@ class UserService:
         for user in self.users:
             if user.user_id == user_id:
                 return user
-
         return None
+
+    def save_users(self):
+
+        data = []
+
+        for user in self.users:
+            data.append({
+                "user_id": user.user_id,
+                "name": user.name,
+                "email": user.email
+            })
+
+        self.storage.save(data)
+
+        print("Users saved successfully.")
+
+    def load_users(self):
+
+        data = self.storage.load()
+
+        self.users = []
+
+        for user_data in data:
+
+            user = User(
+                user_data["user_id"],
+                user_data["name"],
+                user_data["email"]
+            )
+
+            self.users.append(user)
+
+        print("Users loaded successfully.")
